e588609d42300aa633c08d6c6396eb17d2d2a246
little fish <i529561@gmail.com>
2020-10-30 02:14:16 +0800

    add auction

--- src/main/java/org/lf/api/reply/AuctionReply.java
+++ src/main/java/org/lf/api/reply/AuctionReply.java
@@ -4,7 +4,13 @@ import java.io.IOException;
 
 public class AuctionReply extends AbstractReply {
 
-    public AuctionReply(String apiKey) throws IOException {
-        super("https://api.hypixel.net/skyblock/auction?key=" + apiKey + "&player=Player UUID", ReplyType.AUCTION);
+    public enum Type {
+        player,
+        profile,
+        uuid
+    }
+
+    public AuctionReply(String apiKey, Type t, String value) throws IOException {
+        super("https://api.hypixel.net/skyblock/auction?key=" + apiKey + "&" + t.toString() + "=" + value, ReplyType.AUCTION);
     }
 }
---
+++ src/main/java/org/lf/auction/AuctionDecode.java
@@ -0,0 +1,4 @@
+package org.lf.auction;
+
+public class AuctionDecode {
+}
